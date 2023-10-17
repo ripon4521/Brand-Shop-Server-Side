af292828e22e351618ee140c958dd55ca6c8a563
ripon4521 <riponakkondo4521@gmail.com>
2023-10-17 22:54:04 +0600

    create server side skleton

---
+++ index.js
@@ -0,0 +1,62 @@
+
+
+
+const express = require('express')
+
+const app = express()
+
+const cors = require('cors')
+const port = process.env.PORT ||5000;
+
+app.use(cors());
+app.use(express.json())
+
+
+// riponakondo4521
+// NJzGOAF3FlfqNvqu
+
+
+
+
+const { MongoClient, ServerApiVersion  } = require('mongodb');
+const uri = "mongodb+srv://riponakondo4521:NJzGOAF3FlfqNvqu@cluster0.xm8ksdz.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!ddd')
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
